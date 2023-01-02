56014f82df1470d9ff73630f01de9ae19b5c85e3
Mino260806 <dmezghanni@gmail.com>
2023-01-02 22:15:50 +0100

    added utilities to change contrast

---
+++ util.py
@@ -0,0 +1,6 @@
+# https://stackoverflow.com/a/42054155/10231266
+def change_contrast(img, level):
+    factor = (259 * (level + 255)) / (255 * (259 - level))
+    def contrast(c):
+        return 128 + factor * (c - 128)
+    return img.point(contrast)
